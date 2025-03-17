43717e2e117c9b3aaa7e60b69a863ab30c009c14
Gaël Voineau <g.voineau@icloud.com>
2025-03-18 00:09:57 +0100

    initialisation du main.js et création de la class task dans le fichier task.js

---
+++ task.js
@@ -0,0 +1,8 @@
+class task {
+    constructor(name, description = "empty",importanceLvl = 0, categories) {
+        this.name = name;
+        this.description = description;
+        this.importanceLvl = importanceLvl;
+
+    }
+}
